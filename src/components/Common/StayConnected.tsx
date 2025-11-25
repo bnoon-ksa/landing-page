7d9d6043c09dac980fc547b486f3275bb8c90317
dubaiwebcity <dubaiwebcity@gmail.com>
2025-11-25 17:40:45 +0500

    Update StayConnected.tsx

--- src/components/Common/StayConnected.tsx
+++ src/components/Common/StayConnected.tsx
@@ -50,7 +50,7 @@ const HowItWorks = () => {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
-                    placeholder="Enter your email"
+                    placeholder=""
                     className="form-input flex-grow border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                   />
 
