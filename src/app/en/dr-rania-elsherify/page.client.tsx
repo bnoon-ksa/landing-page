bdec2d905371fca13889aad61dfff453836b0543
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-05 20:46:36 +0500

    Add files via upload

---
+++ src/app/en/dr-rania-elsherify/page.client.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Layout/Navbar";
+import DrRaniaElsherify from "@/components/Common/DrRaniaElsherify";
+export default function DrRaniaElsherifyPage() {
+  return (
+    <>
+      <Navbar />
+       <DrRaniaElsherify  />
+
+    </>
+  );
+}
