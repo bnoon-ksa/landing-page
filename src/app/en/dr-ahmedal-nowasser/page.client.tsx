ca3c9c1ddf275f2e028b5d55fbf02cd4d2456b4d
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-06 00:21:19 +0500

    Create page.client.tsx

---
+++ src/app/en/dr-ahmedal-nowasser/page.client.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Layout/Navbar";
+import DrAhmedAlNowasser from "@/components/Common/DrAhmedAlNowasser";
+export default function DrAhmedAlNowasserPage() {
+  return (
+    <>
+      <Navbar />
+       <DrAhmedAlNowasser  />
+
+    </>
+  );
+}
