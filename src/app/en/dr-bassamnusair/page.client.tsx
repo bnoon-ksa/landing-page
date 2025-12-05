7794077bc657658503c166ce6d060092fed64c63
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-05 22:29:27 +0500

    Create page.client.tsx

---
+++ src/app/en/dr-bassamnusair/page.client.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Layout/Navbar";
+import DrBassam  from "@/components/Common/DrBassam";
+export default function DrBassamPage() {
+  return (
+    <>
+      <Navbar />
+       <DrBassam  />
+
+    </>
+  );
+}
