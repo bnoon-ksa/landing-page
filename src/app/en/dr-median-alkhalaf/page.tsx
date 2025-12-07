38ace09a3463879d330603ba7c3626178fb4082f
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-07 19:30:22 +0500

    Add files via upload

---
+++ src/app/en/dr-median-alkhalaf/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+import ClientDrMedianAlkhalafPage from "./page.client";
+
+export const metadata: Metadata = {
+  title: "Dr. Median Al khalaf  – Best Fertility Doctor in Al Ahsa | Bnoon ",
+  description:
+    "",
+};
+
+export default function ServerDrMedianAlkhalafPage() {
+  return <ClientDrMedianAlkhalafPage />;
+}
