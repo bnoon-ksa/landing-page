02314b410a6b4ce615c77909df05952de74ac74f
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-07 16:13:59 +0500

    Create page.client.tsx

---
+++ src/app/dr-median-alkhalaf/page.client.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/features/Layout/Navbar";
+import PageBanner from "@/features/Layout/PageBanner";
+import DrMedianAlkhalaf from "@/features/Common/DrMedianAlkhalaf";
+export default function ClientDrAhmedNowasserPage() {
+  return (
+    <>
+      <Navbar />
+      <PageBanner bgImage="/images/clinics-banner-ar.jpg" />
+       <DrMedianAlkhalaf />
+
+    </>
+  );
+}
