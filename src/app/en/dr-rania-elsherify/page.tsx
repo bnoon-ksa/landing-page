1baf924b8256503fd30ee98384edea2dee089292
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-09 02:08:23 +0500

    Update page.tsx

--- src/app/en/dr-rania-elsherify/page.tsx
+++ src/app/en/dr-rania-elsherify/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import ClientDrRaniaElsherifyPage from "./page.client";
 
 export const metadata: Metadata = {
-  title: "Dr. Rania Elsherify is a highly experienced Obstetrics & Gynecology Registrar at Bnoon – Al Ahsa",
+  title: "Dr. Rania Elsherify - OBGYN Doctor in Bnoon | Al Ahsa",
   description:
     "",
 };
@@ -10,4 +10,5 @@ export const metadata: Metadata = {
 export default function ServerDrRaniaElsherifyPage() {
   return <ClientDrRaniaElsherifyPage />;
 }
+
 
