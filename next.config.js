937c1b475b99137fa17fcc29c0c086fad84e2487
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-09 04:31:24 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
   images: { unoptimized: true },
-
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/ar",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
