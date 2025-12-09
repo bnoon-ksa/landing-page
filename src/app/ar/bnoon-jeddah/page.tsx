bd6257f4ee635aa51a5ec6e3cb362c87af6abae8
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-09 12:25:08 +0500

    Update page.tsx

--- src/app/ar/bnoon-jeddah/page.tsx
+++ src/app/ar/bnoon-jeddah/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import ClientBnoonJeddahPage from "./page.client";
 
 export const metadata: Metadata = {
-  title: "بنون – الشبكة الرائدة لمراكز الإخصاب وصحة المرأة في السعودية ",
+  title: "بنون - مركز الإخصاب وصحة المرأة في جدة",
   description:
    "",
 };
