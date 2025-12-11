5d4bf29f6b5cf0e04f146c1cebbf59b53b56d246
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-11 23:33:51 +0500

    Update page.tsx

--- src/app/en/bnoon-jeddah/page.tsx
+++ src/app/en/bnoon-jeddah/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import ClientBnoonJeddahPage from "./page.client";
 
 export const metadata: Metadata = {
-  title: "Bnoon - Jeddah| The Fertility & Women Health Center",
+  title: "Bnoon - Jeddah | The Fertility & Women Health Center",
   description:
     "",
 };
