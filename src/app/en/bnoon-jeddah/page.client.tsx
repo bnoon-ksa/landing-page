415f7dfa5562dea5c4755675457f140ddcdd53f1
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-11 15:35:35 +0500

    Update page.client.tsx

--- src/app/en/bnoon-jeddah/page.client.tsx
+++ src/app/en/bnoon-jeddah/page.client.tsx
@@ -1,12 +1,12 @@
 import Navbar from "@/components/Layout/Navbar";
 import PageBanner from "@/components/Layout/PageBanner";
-import AlahsaArea from "@/components/Common/AlahsaArea";
-export default function ClientBnoonAlahsaPage() {
+import Jeddaharea from "@/components/Common/Jeddaharea";
+export default function ClientBnoonJeddahPage() {
   return (
     <>
       <Navbar />
       <PageBanner bgImage="/images/clinics-banner.jpg" />
-       <AlahsaArea />
+       <Jeddaharea />
 
     </>
   );
