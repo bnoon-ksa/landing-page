75f93139ca09cc22e6d6ae262ff4ead401b15cb9
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-14 02:48:38 +0500

    Update page.tsx

--- src/app/ar/your-visit/page.tsx
+++ src/app/ar/your-visit/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import ClientYourVisitPage from "./page.client";
 
 export const metadata: Metadata = {
-  title: "بنون – زيارتكم لمراكزنا في الرياض وجدة ",
+  title:  "بنون – زيارتكم لمراكزنا في الرياض وجدة والأحساء ",
   description:
     "",
 };
