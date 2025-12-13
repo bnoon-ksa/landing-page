2b5185f77040a7d9c867b5a69363ac38f0c71e11
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-13 17:09:35 +0500

    Update OurBlog.tsx

--- src/components/Common/OurBlog.tsx
+++ src/components/Common/OurBlog.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import 'remixicon/fonts/remixicon.css';
 
 const OurBlog = () => {
-const [hoveredId, setHoveredId] = useState<number | null>(null);
+  const [hoveredId, setHoveredId] = useState<number | null>(null);
 
   const blogData = [
     {
@@ -14,7 +14,7 @@ const [hoveredId, setHoveredId] = useState<number | null>(null);
       embedMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.1839039641172!2d46.7350649!3d24.789155499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2efddca951caad%3A0xff57eb5bc69f10ea!2zQm5vb24gLSBSaXlhZGgg2KjZhtmI2YYgLSDYp9mE2LHZitin2LY!5e0!3m2!1sen!2s!4v1762873112473!5m2!1sen!2s",
       buttonLink: "en/bnoon-riyadh",
       description:
-        "With a legacy of excellence in fertility and women’s health, Bnoon has been proudly serving families for over 12 years...",
+        "With a legacy of excellence in fertility and women’s health, Bnoon has been proudly serving families for over 12 years in Riyadh. Renowned for its patient-first philosophy, the center combines compassionate care with advanced technology and a team of highly experienced reproductive specialists, earning its reputation as one of the most trusted names in reproductive medicine across Saudi Arabia...",
     },
     {
       id: 2,
@@ -24,26 +24,27 @@ const [hoveredId, setHoveredId] = useState<number | null>(null);
       embedMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3710.6924446189687!2d39.1215956!3d21.558878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c3dbc1b47a93e5%3A0x403eb3afa0ca3bd7!2zQm5vb24gLSBKZWRkYWgg2KjZhtmI2YYgLSDYrNiv2KkgKGZvcm1lcmx5IGtub3duIGFzIEhlYWx0aFBsdXMgRmVydGlsaXR5IEplZGRhaCk!5e0!3m2!1sen!2s!4v1762873494627!5m2!1sen!2s",
       buttonLink: "en/bnoon-jeddah",
       description:
-        "Acquired in June 2025, Bnoon Jeddah — formerly known as HealthPlus Fertility Center Jeddah — has long been recognized for its...",
+        "Acquired in June 2025, Bnoon Jeddah — formerly known as HealthPlus Fertility Center Jeddah — has long been recognized for its medical excellence and the trust it has built within the local community. Now as part of a leading network of fertility centers, the center reflects the group’s commitment to expanding access to world-class fertility and women’s health services...",
     },
     {
       id: 3,
       imageSrc: "images/blog/king-salman.avif",
       title: "Bnoon – King Salman Road, Riyadh ",
-       note: "(Opening December 2025)",
+      note: "(Opening Early 2026)",
       slug: "https://maps.app.goo.gl/F9Qu7tQQp74TTyNv9?g_st=iwb",
       embedMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.3583391235643!2d46.59119140000001!3d24.8174163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2ee69d1c7fb897%3A0x46a86841c1f1d0e8!2zUlJRQTQxNTAsIDQxNTAgS2luZyBTYWxtYW4gQmluIEFiZHVsYXppeiBSZCwgNjkzMtiMINit2Yog2KfZhNmC2YrYsdmI2KfZhtiMIFJpeWFkaCAxMzUzMiwgU2F1ZGkgQXJhYmlh!5e0!3m2!1sen!2s!4v1762873544370!5m2!1sen!2s",
       buttonLink: "en/our-clinics",
       description:
-        "To further expand access and redefine the standard of fertility care in the Kingdom...",
+        "To further expand access and redefine the standard of fertility care in the Kingdom, Bnoon is developing a state-of-the-art 3,800 sqm flagship facility on King Salman Road in North Riyadh, scheduled to open by the end of 2025. Designed as one of the most advanced fertility and women’s...",
     },
-     {
+    {
       id: 4,
       imageSrc: "images/blog/bnoon-alahsa.jpg",
       title: "Bnoon – Al Ahsa ",
-        buttonLink: "en//bnoon-alahsa",
+      embedMap: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3603.0477616735334!2d49.572361099999995!3d25.4366667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDI2JzEyLjAiTiA0OcKwMzQnMjAuNSJF!5e0!3m2!1sen!2s!4v1765622876993!5m2!1sen!2s",
+      buttonLink: "en//bnoon-alahsa",
       description:
-        "Situated within Almoosa Specialist Hospital,<strong> Bnoon – Al Ahsa </strong>brings world-class fertility and women’s health..."
+        "Situated within Almoosa Specialist Hospital,<strong> Bnoon – Al Ahsa </strong>brings world-class fertility and women’s health services to the heart of Saudi Arabia’s Eastern Province. The center is designed to offer an exceptional patient experience, combining advanced reproductive technologies with a compassionate, holistic model of care. As part of Bnoon’s growing national..."
     },
   ];
 
@@ -62,44 +63,44 @@ const [hoveredId, setHoveredId] = useState<number | null>(null);
 
         <div className="row g-4">
           {blogData.map((post) => (
-            <div key={post.id} className="col-lg-4 col-md-6">
+            <div key={post.id} className="col-lg-6 col-md-6">
               <div
                 className="blog-card"
                 onMouseEnter={() => setHoveredId(post.id)}
                 onMouseLeave={() => setHoveredId(null)}
               >
-             <div className="image">
-  <img
-    src={post.imageSrc}
-    alt={post.title}
-    className={hoveredId === post.id ? "fade-out" : "fade-in"}
-  />
-  <iframe
-    src={post.embedMap}
-    width="100%"
-    height="201"
-    style={{ border: "0" }}
-    allowFullScreen
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade"
-    className={hoveredId === post.id ? "fade-in" : "fade-out"}
-  ></iframe>
-</div>
+                <div className="image">
+                  <img
+                    src={post.imageSrc}
+                    alt={post.title}
+                    className={hoveredId === post.id ? "fade-out" : "fade-in"}
+                  />
+                  <iframe
+                    src={post.embedMap}
+                    width="100%"
+                    height="201"
+                    style={{ border: "0" }}
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    className={hoveredId === post.id ? "fade-in" : "fade-out"}
+                  ></iframe>
+                </div>
 
 
                 <div className="content">
                   <h3>
-                     <i className="ri-map-pin-line" style={{ color: "#004E78", marginRight: "6px" }}></i>
+                    <i className="ri-map-pin-line" style={{ color: "#004E78", marginRight: "6px" }}></i>
                     <a href={post.slug} target="_blank" rel="noopener noreferrer">
                       {post.title}
-                       {post.note && <span className="small-note">{post.note}</span>}
+                      {post.note && <span className="small-note">{post.note}</span>}
                     </a>
                   </h3>
-                 <p
-  dangerouslySetInnerHTML={{
-    __html: post.description
-  }}
-></p>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.description
+                    }}
+                  ></p>
 
 
                   <Link href={post.buttonLink} className="btn btn-success btn-blog">
@@ -111,12 +112,18 @@ const [hoveredId, setHoveredId] = useState<number | null>(null);
           ))}
         </div>
       </div>
-        {/* ✅ Responsive style only for mobile */}
-        <style jsx>{`
+      {/* ✅ Responsive style only for mobile */}
+      <style jsx>{`
+      .blog-card .content p {
+  font-size: 16px !important;      /* font size kam */
+  line-height: 1.5;    /* line height kam */
+}
+
         .small-note {
   font-size: 16px; /* smaller size for note */
   color: #004E78;
-  margin-left: 32px;
+  margin-left: 26px;
+   display: block;
 }
 
         .blog-card .content h3, .blog-card .content .h3 {
@@ -131,7 +138,7 @@ const [hoveredId, setHoveredId] = useState<number | null>(null);
 }
   .blog-card .image {
     position: relative;
-    height: 201px;
+    height: 320px;
     overflow: hidden;
   }
 
@@ -163,6 +170,15 @@ const [hoveredId, setHoveredId] = useState<number | null>(null);
     transition:none;
     color: #004E78;
     margin-top: 8px;
+}
+     .blog-card .image {
+    position: relative;
+    height: 220px;
+    overflow: hidden;
+  }
+          .blog-card .content p {
+  font-size: 14px !important;      /* font size kam */
+  line-height: 1.5;    /* line height kam */
 }
            .small-note {
   font-size: 13px; /* smaller size for note */
