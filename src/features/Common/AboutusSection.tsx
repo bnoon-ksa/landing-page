fdcac2398999a508c77e57afce3281806045d9df
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-13 14:52:17 +0500

    Update AboutusSection.tsx

--- src/features/Common/AboutusSection.tsx
+++ src/features/Common/AboutusSection.tsx
@@ -7,10 +7,10 @@ import { motion, Easing, easeOut } from "framer-motion";
 const AboutusSection = () => {
   const values = [
     "التعاطف",
-    "النزاهة",
-    "الابتكار",
-    "التعاون والعمل الجماعي",
-    "القيادة والإلهام",
+    "النزاهة  ",
+    "الابتكار  ",
+    "التعاون والعمل الجماعي  ",
+    "القيادة والإلهام  ",
   ];
 // ⭐ Animation variant
   const slideInRight = {
@@ -23,34 +23,33 @@ const AboutusSection = () => {
   };
 
   return (
-    <div className="fertility-area mt-lg-5">
+    <div className="fertility-area mt-3 mt-lg-5">
       <div className="container">
         <div className="section-title">
           <div className="row justify-content-center align-items-center g-4">
             <div className="col-lg-12 col-md-12">
               <div className="left">
-                <h2 className="mb-2">رؤيتنا</h2>
+                <h2 className="mb-2">رؤيتنا </h2>
                <motion.p
                   initial="hidden"
                   whileInView="visible"
                   viewport={{ once: true, amount: 0.3 }}
                   variants={slideInRight}
                 >
-                 نلتزم في بنون بتقديم الرعاية الأمثل والدعم لكل من لديه حلم الأمومة والأبوة وتكوين أسرة صحية.
+               نسعى لإحداث نقلة نوعية في مجال خدمات الإخصاب وعلم الوراثة الإنجابية في المنطقة، والانطلاق نحو الريادة العالمية. سنحقق ذلك من خلال البحث والابتكار، وتبني أحدث التقنيات الطبية، وبناء شراكات مع مؤسسات عالمية مرموقة، ووضع المريض في المقام الأول. 
                 </motion.p>
               </div>
             </div>
 
             <div className="col-lg-12 col-md-12">
               <div className="left">
-                <h2 className="mb-2">مهمتنا</h2>
+                <h2 className="mb-2">مهمتنا </h2>
                 <motion.p
                   initial="hidden"
                   whileInView="visible"
                   viewport={{ once: true, amount: 0.3 }}
                   variants={slideInRight}
-                >
-               نسعى لإحداث نقلة نوعية في مجال خدمات الإخصاب وعلم الوراثة الإنجابية في منطقتنا، والانطلاق نحو الريادة العالمية. نحقق ذلك من خلال الاستثمار في البحث والابتكار، وتبني أحدث التقنيات الطبية، وبناء شراكات مع مؤسسات عالمية مرموقة، ووضع المريض واحتياجاته في المقام الأول۔    </motion.p>
+                >نلتزم في بنون بدعم وتقديم الرعاية الأمثل لكل من لديه حلم الأمومة والأبوة وبناء أسرة صحية. </motion.p>
               </div>
             </div>
 
@@ -64,7 +63,7 @@ const AboutusSection = () => {
                   viewport={{ once: true, amount: 0.3 }}
                   variants={slideInRight}
                 >
-              قيمنا هي البوصلة التي توجهنا، والركيزة التي تُبنى عليها ثقافتنا وتتضمن : </motion.p>
+             قيمنا هي البوصلة التي توجهنا، والركيزة التي تُبنى عليها ثقافتنا وتتضمن:</motion.p>
                 <ul className="values-list mt-3">
                   {values.map((value, index) => (
                     <li key={index} className="d-flex align-items-center mb-2">
