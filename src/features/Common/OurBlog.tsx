0e2113155b800c6bf61d32328494c01e74f28b64
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-13 22:41:59 +0500

    Update OurBlog.tsx

--- src/features/Common/OurBlog.tsx
+++ src/features/Common/OurBlog.tsx
@@ -12,41 +12,37 @@ const OurBlog = () => {
       slug: "https://maps.app.goo.gl/Uwu7B8FT8n7bYNid6", // Maps link
       embedMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.1839039641172!2d46.7350649!3d24.789155499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2efddca951caad%3A0xff57eb5bc69f10ea!2zQm5vb24gLSBSaXlhZGgg2KjZhtmI2YYgLSDYp9mE2LHZitin2LY!5e0!3m2!1sen!2s!4v1762873112473!5m2!1sen!2s",
       buttonLink: "ar/bnoon-riyadh", // internal page
-      description:
-        "تُعد بنون من المراكز الرائدة في علاجات الإخصاب وصحة المرأة والرجل في العاصمة منذ افتتاحه عام 2013. يضم المركز أحدث التقنيات والمرافق ويتميز بنسب نجاح عالية على يد كادر طبي متميز۔",
+      description: `على مدار أكثر من 12 عاماً، استطاع مركز "بنون" في الرياض أن يرسّخ مكانته كأحد أكثر المراكز موثوقية في طب الخصوبة وصحة المرأة والرجل في المملكة. من خلال نهج يضع المريض أولاً، يجمع المركز بين الرعاية الإنسانية والتكنولوجيا المتقدمة، ويضم فريقاً من أبرز الاستشاريين في طب الإخصاب، ما جعله وجهة مفضّلة للعائلات الباحثة عن رعاية متخصصة ونتائج ملموسة.`,
     },
     {
       id: 2,
       imageSrc: "images/blog/bnoon-jeddah.avif",
       title: "بنون – جدة",
       slug: "https://www.google.com/maps/place/HealthPlus+Fertility+%26+Women's+Health+Center+-+Jeddah/data=!4m2!3m1!1s0x0:0x403eb3afa0ca3bd7?sa=X&ved=1t:2428&ictx=111",
-      embedMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3710.6924446189687!2d39.1215956!3d21.558878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c3dbc1b47a93e5%3A0x403eb3afa0ca3bd7!2zQm5vb24gLSBKZWRkYWgg2KjZhtmI2YYgLSDYrNiv2KkgKGZvcm1lcmx5IGtub3duIGFzIEhlYWx0aFBsdXMgRmVydGlsaXR5IEplZGRhaCk!5e0!3m2!1sen!2s!4v1762873494627!5m2!1sen!2s", 
+      embedMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3710.6924446189687!2d39.1215956!3d21.558878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c3dbc1b47a93e5%3A0x403eb3afa0ca3bd7!2zQm5vb24gLSBKZWRkYWgg2KjZhtmI2YYgLSDYrNiv2KkgKGZvcm1lcmx5IGtub3duIGFzIEhlYWx0aFBsdXMgRmVydGlsaXR5IEplZGRhaCk!5e0!3m2!1sen!2s!4v1762873494627!5m2!1sen!2s",
       buttonLink: "ar/bnoon-jeddah",
-      description:
-        "انضم مركز بنون في جدة (المعروف سابقاً بمركزهيلث بلاس للإخصاب) إلى الشبكة في يونيو 2025 لتقديم خدمات موسّعة للعائلات في المنطقة الغربية بفرق  طبية متخصصة۔",
-    },
+    description: `انضم إلى شبكة "بنون" في يونيو 2025 (وكان يُعرف سابقاً بمركز "هيلث بلس للإخصاب – جدة" منذ افتتاحه عام 2019)، ويواصل المركز تقديم خدمات رعاية خصوبة متقدمة، مع الحفاظ على السمعة الطبية المرموقة والثقة التي بناها في المجتمع. وبصفته مركزاً معتمداً من اللجنة الدولية المشتركة، يقدّم "بنون – جدة" مجموعة متكاملة من الخدمات، تشمل: أطفال الأنابيب، الحقن المجهري، حفظ الخصوبة، `,
+      },
     {
       id: 3,
       imageSrc: "images/blog/king-salman.avif",
-  title: `بنون – طريق الملك سلمان، الرياض <br /><span style="font-size:14px; font-weight:400; color:#004E78;">(الافتتاح في نهاية 2025)</span>`,
+      title: `بنون – طريق الملك سلمان، الرياض <br /><span style="font-size:14px; font-weight:400; color:#004E78;">(الافتتاح: ديسمبر 2025)</span>`,
 
       slug: "https://maps.app.goo.gl/F9Qu7tQQp74TTyNv9?g_st=iwb",
-        embedMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.3583391235643!2d46.59119140000001!3d24.8174163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2ee69d1c7fb897%3A0x46a86841c1f1d0e8!2zUlJRQTQxNTAsIDQxNTAgS2luZyBTYWxtYW4gQmluIEFiZHVsYXppeiBSZCwgNjkzMtiMINit2Yog2KfZhNmC2YrYsdmI2KfZhtiMIFJpeWFkaCAxMzUzMiwgU2F1ZGkgQXJhYmlh!5e0!3m2!1sen!2s!4v1762873544370!5m2!1sen!2s",
+      embedMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.3583391235643!2d46.59119140000001!3d24.8174163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2ee69d1c7fb897%3A0x46a86841c1f1d0e8!2zUlJRQTQxNTAsIDQxNTAgS2luZyBTYWxtYW4gQmluIEFiZHVsYXppeiBSZCwgNjkzMtiMINit2Yog2KfZhNmC2YrYsdmI2KfZhtiMIFJpeWFkaCAxMzUzMiwgU2F1ZGkgQXJhYmlh!5e0!3m2!1sen!2s!4v1762873544370!5m2!1sen!2s",
       buttonLink: "ar/our-clinics",
-      description:
-        "مرفق رائد تبلغ مساحته 3,800 متر مربع ويضم 4 طوابق، صُمّم ليجسّد المستقبل الواعد لرعاية الخصوبة باستخدام الذكاء الاصطناعي والتكنولوجيا المتقدمة۔",
-    },
-     {
+        description: `ضمن خطتها لتوسيع الوصول إلى علاجات الإخصاب عالية المستوى في المملكة، تعمل "بنون" على تطوير منشأة طبية متكاملة بمساحة 3,800 متر مربع في شمال الرياض على طريق الملك سلمان، والمقرّر افتتاحها نهاية عام 2025. هذا المركز الرائد صُمّم ليكون من بين أكثر مراكز الخصوبة تطوراً في المنطقة،`,
+     },
+    {
       id: 4,
       imageSrc: "images/blog/bnoon-alahsa.jpg",
-  title:` بنون – الأحساء`,
+      title: ` بنون – الأحساء`,
 
-      slug: "",
-        embedMap: "",
+      slug: "https://maps.app.goo.gl/N4qVpDH22TnbpVDV7",
+      embedMap: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3603.0477616735334!2d49.572361099999995!3d25.4366667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDI2JzEyLjAiTiA0OcKwMzQnMjAuNSJF!5e0!3m2!1sen!2s!4v1765645696414!5m2!1sen!2s",
       buttonLink: "ar/bnoon-alahsa",
-      description:
-       "يقدّم مركز بنون – الأحساء الواقع داخل مستشفى الموسى التخصصي خدمات رعاية الخصوبة وصحة المرأة بمعايير عالمية، ليكون الوجهة الأولى للعائلات في المنطقة الشرقية في السعودية.",
-    },
+       description: `يقدّم مركز بنون – الأحساء الواقع داخل مستشفى الموسى التخصصي خدمات رعاية الخصوبة وصحة المرأة بمعايير عالمية، ليكون الوجهة الأولى للعائلات في المنطقة الشرقية في السعودية. وقد صُمّم المركز ليمنح المرضى تجربة علاجية متميّزة تجمع بين أحدث التقنيات في الطب الإنجابي ورعاية متكاملة تُعنى بكل تفاصيل رحلة الزوجين. ويمثّل هذا المركز جزءاً من شبكة بنون المتنامية، والتي تهدف`,
+       },
   ];
 
   return (
@@ -64,60 +60,64 @@ const OurBlog = () => {
 
         <div className="row g-4">
           {blogData.map((post) => (
-            <div key={post.id} className="col-lg-4 col-md-6">
-               <div
+            <div key={post.id} className="col-lg-6 col-md-6">
+              <div
                 className="blog-card"
                 onMouseEnter={() => setHoveredId(post.id)}
                 onMouseLeave={() => setHoveredId(null)}
               >
-             
+
                 {/* Image links to Google Maps */}
-          <div className="image">
-            {hoveredId === post.id ? (
-  <iframe
-    src={post.embedMap} // <-- yahi har card ka map show karega
-    width="100%"
-    height="200"
-    style={{ border: "0" }}
-    allowFullScreen
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade"
-  ></iframe>
-) : (
-  <a href={post.slug} target="_blank" rel="noopener noreferrer">
-    <img src={post.imageSrc} alt={post.title} />
-  </a>
-)}
+                <div className="image">
+                  {hoveredId === post.id ? (
+                    <iframe
+                      src={post.embedMap} // <-- yahi har card ka map show karega
+                      width="100%"
+                      height="200"
+                      style={{ border: "0" }}
+                      allowFullScreen
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  ) : (
+                    <a href={post.slug} target="_blank" rel="noopener noreferrer">
+                      <img src={post.imageSrc} alt={post.title} />
+                    </a>
+                  )}
 
                 </div>
 
 
                 {/* Heading links to Google Maps */}
                 <div className="content">
-                 <h3>
+                  <h3>
                     <i className="ri-map-pin-line" style={{ color: "#004E78", marginLeft: "8px" }}></i>
-  <a
-    href={post.slug}
-    target="_blank"
-    rel="noopener noreferrer"
-    dangerouslySetInnerHTML={{ __html: post.title }}
-  ></a>
-</h3>
+                    <a
+                      href={post.slug}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      dangerouslySetInnerHTML={{ __html: post.title }}
+                    ></a>
+                  </h3>
 
                   <p>{post.description}</p>
 
                   {/* Button links to internal page */}
                   <Link href={post.buttonLink} className="btn btn-success btn-blog">
-                   اقرأ المزيد
+                    اقرأ المزيد
                   </Link>
                 </div>
-               
+
               </div>
             </div>
           ))}
         </div>
       </div>
-       <style jsx global>{`
+      <style jsx global>{`
+            .blog-card .content p {
+  font-size: 15px !important;      /* font size kam */
+  line-height: 1.5;    /* line height kam */
+}
        .small-text {
   font-size: 16px; /* smaller size for note */
   color: #004E78;
@@ -133,15 +133,29 @@ const OurBlog = () => {
     flex-direction: column;
     justify-content: space-between;
     height: 100%; /* important for same height in grid */
-    min-height: 400px; /* adjust according to design */
+    min-height: 320px; /* adjust according to design */
 }
 
 /* Make image area fixed height */
 .blog-card .image {
-    height: 200px; /* all images/iframes same height */
+    height: 300px; /* all images/iframes same height */
     overflow: hidden;
 }
-
+.blog-card .image img {
+    width: 100%;
+    height: auto; /* natural ratio maintain */
+    display: block;
+}
+.blog-card .image {
+    height: 300px; /* desktop height */
+    overflow: hidden;
+}
+.blog-card .image img,
+.blog-card .image iframe {
+    width: 100%;
+    height: 100%; /* fills .image container */
+    display: block;
+}
 /* Content area fills remaining space */
 .blog-card .content {
     flex: 1; /* ensures content area grows to fill height */
@@ -151,6 +165,17 @@ const OurBlog = () => {
      .blog-card .content h3 {
         font-size: 14px;
     }
+        .blog-card .image {
+    height: 200px; /* all images/iframes same height */
+    overflow: hidden;
+}
+     .blog-card .image {
+        height: 200px; /* mobile height */
+    }
+                   .blog-card .content p {
+  font-size: 12px !important;      /* font size kam */
+  line-height: 1.5;    /* line height kam */
+}
   }
 `}</style>
     </div>
