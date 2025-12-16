e11ea1618b38ebe9c2caad3f7a01e36feaa839d9
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-16 23:24:43 +0500

    Update page.client.tsx

--- src/app/en/patients-rights/page.client.tsx
+++ src/app/en/patients-rights/page.client.tsx
@@ -5,7 +5,7 @@ import PaitentRights from "@/components/Common/PaitentRights";
 export default function PatientsRightsPage() {
   return (
    
-    <div className="patient-rights-page no-footer">
+    <div className="">
        <Navbar />
       <PaitentRights />
     </div>
