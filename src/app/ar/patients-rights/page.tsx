dffe8c5798a90eac954b7030181458b77d2ddf8e
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-17 00:27:16 +0500

    Add files via upload

---
+++ src/app/ar/patients-rights/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+import ClientPatientsRightsPage from "./page.client";
+
+export const metadata: Metadata = {
+  title: "Patients Rights – Bnoon",
+  description: "",
+};
+
+export default function ServerPatientsRightsPage() {
+  return <ClientPatientsRightsPage />;
+}
