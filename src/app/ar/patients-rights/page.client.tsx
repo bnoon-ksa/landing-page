9655eab64d16bd4b9f612919a3a80d50c497de6f
dubaiwebcity <dubaiwebcity@gmail.com>
2025-12-16 23:53:23 +0500

    Create page.client.tsx

---
+++ src/app/ar/patients-rights/page.client.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Navbar from "@/features/Layout/Navbar";
+import PaitentRights from "@/components/Common/PaitentRights";
+
+export default function PatientsRightsPage() {
+  return (
+   
+    <div className="patient-rights-page no-footer">
+       <Navbar />
+      <PaitentRights />
+    </div>
+  );
+}
