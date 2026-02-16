cd6e8a33c0a0e8aa28b1e87e640448fea70fece0
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-16 12:57:53 +0500

    Update NationalDayOffer.tsx

--- src/components/Common/NationalDayOffer.tsx
+++ src/components/Common/NationalDayOffer.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -262,3 +261,4 @@ const NationalDayOffer = () => {
 };
 
 export default NationalDayOffer;
+
