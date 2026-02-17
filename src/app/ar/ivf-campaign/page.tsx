eb8cce53f1a3d16fac8c2a5220c192e029c521cb
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-17 20:11:09 +0500

    Update page.tsx

--- src/app/ar/ivf-campaign/page.tsx
+++ src/app/ar/ivf-campaign/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
+import PageBanner from "@/components/ar/Layout/PageBanner";
 import Navbar from "@/components/ar/Layout/Navbar";
-import PageBanner from "@/components/ar/Layout/FoundingPageBanner";
 import NationalDayOffer from "@/components/ar/Common/NationalDayOffer";
 
 export const metadata: Metadata = {
@@ -14,7 +14,31 @@ export default function FoundingDayDiscountPage() {
   return (
     <>
       <Navbar />
-      <PageBanner />
+        <div style={{ position: "relative" }}>
+  <PageBanner bgImage="/images/static-banner-ar.jpeg" />
+
+  <div
+    className="second-banner-content reveal-text"
+    style={{
+      position: "absolute",
+      top: "50%",
+      right: "8%",
+      transform: "translateY(-50%)",
+      zIndex: 10,
+    }}
+  >
+    <h1 style={{ color: "#004E78" }}>
+      <span className="rowdies-font">
+       العرض الخاص بيوم التأسيس السعودي 
+      </span>
+    </h1>
+
+    <p style={{ color: "#fff" }} className="special-desc"></p>
+
+    <p style={{ color: "#fff" }} className="terms-text"></p>
+  </div>
+</div>
+
       <NationalDayOffer />
     </>
   );
