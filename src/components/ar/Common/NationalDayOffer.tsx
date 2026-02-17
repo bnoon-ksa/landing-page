ecfc313b56d54b4c40cb1871df5883260c028009
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-17 17:06:37 +0500

    Update NationalDayOffer.tsx

--- src/components/ar/Common/NationalDayOffer.tsx
+++ src/components/ar/Common/NationalDayOffer.tsx
@@ -1,11 +1,12 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const NationalDayOffer = () => {
   // Alag images for each section
-  const imageRiyadh = "/images/national-day/infertility-treatments.avif";
-  const imageJeddah = "/images/national-day/icsi-cycles.avif";
+  const imageRiyadh = "/images/national-day/men-infertility.jpg";
+  const imageJeddah = "/images/national-day/ivf-cycles.jpg";
 
   const includesList = [
     "متابعة التبويض بالسونار ",
@@ -24,10 +25,10 @@ const NationalDayOffer = () => {
               <h2>العرض الخاص بيوم التأسيس السعودي</h2>
             </div>
             <div className="service-overview-content text-center national-box">
-              <h2 className="national-text">خصم على الدورة العلاجية لأطفال الأنابيب والحقن المجهري </h2>
+              <h2 className="national-text ivf-heading">خصم على الدورة العلاجية لأطفال الأنابيب والحقن المجهري </h2>
             </div>
             <div className="service-overview-content text-center national-box-2 mt-3">
-              <h5 className="national-text-2">بنون - الريــــاض | جــــــــدة | الأحســـــاء</h5>
+              <h5 className="national-text-2 ivf-heading">بنون - الريــــاض | جــــــــدة | الأحســـــاء</h5>
             </div>
           </div>
         </div>
@@ -110,7 +111,7 @@ const NationalDayOffer = () => {
                     alt="Phone"
                     width={14}
                     height={14}
-                    className="me-2"
+                    className="icon-margin"
                   />
                   <span>بنون - الرياض:0114448080</span>
                 </div>
@@ -120,7 +121,7 @@ const NationalDayOffer = () => {
                     alt="Phone"
                     width={14}
                     height={14}
-                    className="me-2"
+                    className="icon-margin"
                   />
                   <span>بنون - جدة: 012 680 0800</span>
                 </div>
@@ -130,7 +131,7 @@ const NationalDayOffer = () => {
                     alt="Phone"
                     width={14}
                     height={14}
-                    className="me-2"
+                    className="icon-margin"
                   />
                   <span>بنون – الأحساء: 0552701553</span>
                 </div>
@@ -160,90 +161,52 @@ const NationalDayOffer = () => {
         </div>
 
         {/* Jeddah Section */}
-        <div className="row justify-content-center align-items-center g-4 mt-5 mb-3">
-          <div className="col-xl-6 col-md-12">
-            <div className="service-overview-content text-center national-box">
-              <h2 className="national-text">
-              خصم على خدمات وإجراءات أمراض الذكورة والعقم لدى الرجال
-              </h2>
-            </div>
-            <div className="service-overview-content text-center national-box-2 mt-3">
-              <h5 className="national-text-2">بنــــــــــــــون  
-
- الريــــاض | جــــــــدة  </h5>
-            </div>
-          </div>
-        </div>
-
-        <div className="row justify-content-center align-items-center g-4">
-             <div className="col-xl-5 col-md-12">
-            <div
-              className="service-overview-image mb-3"
-              style={{
-                boxShadow: "50px 50px 0px #d7f2fb",
-                overflow: "hidden",
-                display: "inline-block",
-              }}
-            >
-              <Image
-                src={imageRiyadh}
-                alt="Service overview"
-                width={580}
-                height={450}
-              />
-            </div>
-          </div>
-          <div className="col-xl-5 col-md-12">
-            <div className="service-overview-content national-list">
-              <p className="national-p">
-              يسعدنا في بنـــــون تقديم 
-<strong>خصم 20%</strong>
-على خدمات وإجراءات أمراض الذكورة والعقم لدى الرجال*
-في مراكزنا بجــــدة والريــــاض.
-              </p>
-              <p className="national-p">
-               العرض قائم من{" "}
-                <strong>11 فبراير وحتى 4 مارس 2026. </strong>
-                <br />
-             لا يشمل العرض الاستشارات الطبية
-              </p>
-              <p className="national-p">*تطبق الشروط والأحكام. لا يسري هذا العرض مع أي عروض أخرى قائمة. </p>
-
-              <Link
-                href="https://book.bnoon.sa/ar"
-                className="btn btn-primary contact-btn"
-              >
-               احجز موعدك اليوم
-              </Link>
-
-              <div className="d-flex flex-column mt-3 gap-2">
-                <div className="d-flex align-items-center">
-                  <Image
-                    src="/images/icons/phone.svg"
-                    alt="Phone"
-                    width={14}
-                    height={14}
-                    className="me-2"
-                  />
-                  <span>بنون - جدة: 0126800800</span>
-                </div>
-                  <div className="d-flex align-items-center">
-                  <Image
-                    src="/images/icons/phone.svg"
-                    alt="Phone"
-                    width={14}
-                    height={14}
-                    className="me-2"
-                  />
-                  <span>بنون – الرياض: 0114448080 </span>
-                </div>
-              </div>
-            </div>
-          </div>
-
        
-        </div>
+
+  
       </div>
+       <style jsx global>{`
+        .icon-margin{
+        margin-left: 10px;
+        }
+      
+ .national-p{
+ color:#000;
+ font-size: 16px !important;
+ }
+ .national-overview-image{
+ boxShadow: "-50px 50px 0px #d7f2fb",
+    overflow: "hidden",
+    display: "inline-block",
+    margin: "0 0px 0 0px",
+    }
+  /* Mobile tweaks */
+  @media (max-width: 768px) {
+    .national-text-3 {
+      font-size: 18px;
+      color:#fff;
+    }
+          .btn-appointment {
+        width: 120px;
+    }
+         .national-p{
+ color:#000;
+ font-size: 14px !important;
+ }
+      .national-text-2 {
+    font-size: 16px;
+    text-align: center;
+    margin: 10px 0px;
+}
+    .national-overview-image{
+        margin: 0 0px 0 50px !important;
+
+    }
+          .ivf-heading{
+        font-size: 14px !important; 
+        }
+  }
+`}</style>
     </div>
   );
 };
