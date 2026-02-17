a663ab2b1bff1022a1ca3ce94cd10b090fed8ca6
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-17 14:07:19 +0500

    Add files via upload

--- src/components/ar/Common/NationalDayOffer.tsx
+++ src/components/ar/Common/NationalDayOffer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import { getBookNowUrl } from "@/utils/booking";
 import Image from "next/image";
 
 const NationalDayOffer = () => {
@@ -9,8 +8,8 @@ const NationalDayOffer = () => {
   const imageJeddah = "/images/national-day/icsi-cycles.avif";
 
   const includesList = [
-    "متابعة التبويض بالسونار",
-    "التحاليل الهرمونية أثناء الدورة العلاجية",
+    "متابعة التبويض بالسونار ",
+    "التحاليل الهرمونية أثناء الدورة العلاجية ",
   ];
 
   const excludesList = ["الأدوية", "الفحوصات الجينية"];
@@ -22,13 +21,13 @@ const NationalDayOffer = () => {
         <div className="row justify-content-center align-items-center g-4 mb-3">
           <div className="col-xl-6 col-md-12">
             <div className="service-overview-content text-center">
-              <h2>العرض الخاص باليوم الوطني</h2>
+              <h2>العرض الخاص بيوم التأسيس السعودي</h2>
             </div>
             <div className="service-overview-content text-center national-box">
-              <h2 className="national-text">خصم على الدورة العلاجية لأطفال الأنابيب والحقن المجهري</h2>
+              <h2 className="national-text">خصم على الدورة العلاجية لأطفال الأنابيب والحقن المجهري </h2>
             </div>
             <div className="service-overview-content text-center national-box-2 mt-3">
-              <h5 className="national-text-2">بنون - الرياض وجدة</h5>
+              <h5 className="national-text-2">بنون - الريــــاض | جــــــــدة | الأحســـــاء</h5>
             </div>
           </div>
         </div>
@@ -38,13 +37,15 @@ const NationalDayOffer = () => {
           <div className="col-xl-5 col-md-12">
             <div className="service-overview-content national-list">
               <p className="national-p">
-          <strong>بمناسبة اليوم الوطني السعودي الـ95، يسعدنا في بنون تقديم خصم 15%</strong> على تكلفة الدورة العلاجية لأطفال الأنابيب والحقن المجهري*
+         بمناسبة يوم التأسيس السعودي، يسعدنا في بنــــون أن نقدم 
+<strong> خصم 15% </strong>
+على تكلفة الدورة العلاجية لأطفال الأنابيب والحقن المجهري*
 
               </p>
 
               {/* ✅ Includes */}
               <div className="national-p">
-                <strong>:يشمل</strong>
+                <strong>يشمل: </strong>
                 <ul className="list-unstyled mt-2">
                   {includesList.map((item, index) => (
                     <li
@@ -66,7 +67,7 @@ const NationalDayOffer = () => {
 
               {/* ✅ Excludes */}
               <div className="national-p">
-                <strong>:لا يشمل</strong>
+                <strong>لا يشمل:</strong>
                 <ul className="list-unstyled mt-2">
                   {excludesList.map((item, index) => (
                     <li
@@ -88,15 +89,15 @@ const NationalDayOffer = () => {
 
               <p className="national-p">
                العرض قائم من{" "}
-                <strong> 15 سبتمبر وحتى 15 أكتوبر 2025۔</strong>
+                <strong>11 فبراير وحتى 4 مارس 2026</strong>
                 <br />
                بدء الدورة العلاجية قبل نهاية يوم{" "}
-                <strong> 15 نوفمبر 2025۔</strong>
+                <strong> 2 أبريل 2026. </strong>
               </p>
-              <p className="national-p">تطبق الشروط والأحكام۔*</p>
+              <p className="national-p">*تطبق الشروط والأحكام. لا يسري هذا العرض مع أي عروض أخرى قائمة. </p>
 
               <Link
-                href={getBookNowUrl("ar")}
+                href="https://book.bnoon.sa/ar"
                 className="btn btn-primary contact-btn"
               >
               احجز موعدك اليوم
@@ -122,6 +123,16 @@ const NationalDayOffer = () => {
                     className="me-2"
                   />
                   <span>بنون - جدة: 012 680 0800</span>
+                </div>
+                 <div className="d-flex align-items-center">
+                  <Image
+                    src="/images/icons/phone.svg"
+                    alt="Phone"
+                    width={14}
+                    height={14}
+                    className="me-2"
+                  />
+                  <span>بنون – الأحساء: 0552701553</span>
                 </div>
               </div>
             </div>
@@ -153,11 +164,13 @@ const NationalDayOffer = () => {
           <div className="col-xl-6 col-md-12">
             <div className="service-overview-content text-center national-box">
               <h2 className="national-text">
-               خصم على خدمات وإجراءات أمراض الذكورة والعقم لدى الرجال
+              خصم على خدمات وإجراءات أمراض الذكورة والعقم لدى الرجال
               </h2>
             </div>
             <div className="service-overview-content text-center national-box-2 mt-3">
-              <h5 className="national-text-2">بنون – جدة</h5>
+              <h5 className="national-text-2">بنــــــــــــــون  
+
+ الريــــاض | جــــــــدة  </h5>
             </div>
           </div>
         </div>
@@ -183,19 +196,21 @@ const NationalDayOffer = () => {
           <div className="col-xl-5 col-md-12">
             <div className="service-overview-content national-list">
               <p className="national-p">
-                <strong>يسرنا في بنون – جدة تقديم خصم 20%</strong> على خدمات وإجراءات أمراض الذكورة والعقم لدى الرجال*۔
-
+              يسعدنا في بنـــــون تقديم 
+<strong>خصم 20%</strong>
+على خدمات وإجراءات أمراض الذكورة والعقم لدى الرجال*
+في مراكزنا بجــــدة والريــــاض.
               </p>
               <p className="national-p">
                العرض قائم من{" "}
-                <strong> 15 سبتمبر وحتى 15 أكتوبر 2025۔</strong>
+                <strong>11 فبراير وحتى 4 مارس 2026. </strong>
                 <br />
              لا يشمل العرض الاستشارات الطبية
               </p>
-              <p className="national-p">تطبق الشروط والأحكام۔*</p>
+              <p className="national-p">*تطبق الشروط والأحكام. لا يسري هذا العرض مع أي عروض أخرى قائمة. </p>
 
               <Link
-                href={getBookNowUrl("ar")}
+                href="https://book.bnoon.sa/ar"
                 className="btn btn-primary contact-btn"
               >
                احجز موعدك اليوم
@@ -211,6 +226,16 @@ const NationalDayOffer = () => {
                     className="me-2"
                   />
                   <span>بنون - جدة: 0126800800</span>
+                </div>
+                  <div className="d-flex align-items-center">
+                  <Image
+                    src="/images/icons/phone.svg"
+                    alt="Phone"
+                    width={14}
+                    height={14}
+                    className="me-2"
+                  />
+                  <span>بنون – الرياض: 0114448080 </span>
                 </div>
               </div>
             </div>
