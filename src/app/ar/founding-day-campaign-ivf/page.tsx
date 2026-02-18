ef51538160465616425f42b0f83df8ff323b4d13
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-18 17:07:58 +0500

    Update page.tsx

--- src/app/ar/founding-day-campaign-ivf/page.tsx
+++ src/app/ar/founding-day-campaign-ivf/page.tsx
@@ -16,19 +16,19 @@ export default function FoundingDayDiscountPage() {
       <Navbar />
         <div style={{ position: "relative" }}>
   <FoundingPageBanner bgImage="/images/static-banner-ar.jpeg" />
-
+<div
+    className="container">
   <div
-    className="second-banner-content reveal-text"
+    className="second-banner-content reveal-text text-banner"
     style={{
       position: "absolute",
       top: "50%",
-      right: "8%",
       transform: "translateY(-50%)",
       zIndex: 10,
     }}
   >
     <h1 style={{ color: "#004E78" }}>
-      <span className="rowdies-font">
+      <span className="rowdies-font text-size">
       عرض يوم التأسيس للحقن المجهري
       </span>
     </h1>
@@ -38,7 +38,7 @@ export default function FoundingDayDiscountPage() {
     <p style={{ color: "#fff" }} className="terms-text"></p>
   </div>
 </div>
-
+</div>
       <NationalDayOffer />
     </>
   );
