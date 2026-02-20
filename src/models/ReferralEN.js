d28acb8b1321d536a267322b6115b90133e95c75
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-20 19:45:01 +0500

    Add files via upload

---
+++ src/models/ReferralEN.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const ReferralSchema = new mongoose.Schema(
+  {
+    referTo: { type: String, default: "" },
+
+    physicianName: { type: String, required: true },
+    physicianPhone: { type: String, required: true },
+    physicianEmail: { type: String, default: "" },
+    healthcareFacilityName: { type: String, default: "" },
+    organizationCity: { type: String, default: "" },
+
+    patientName: { type: String, required: true },
+    patientPhone: { type: String, required: true },
+    gender: { type: String, required: true },
+
+    reasons: { type: [String], default: [] },
+    medicalReason: { type: String, default: "" },
+
+    recaptcha: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.ReferralEN || mongoose.model("ReferralEN", ReferralSchema);
