53dcf98cd33887141e6ed4a2e1fa7867305ef7bb
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-21 15:08:09 +0500

    fix: add page (#120)

---
+++ src/app/ar/refer-a-patient/page.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import OptimizedPageBanner from '@/components/ui/OptimizedPageBanner';
+import Navbar from '@/components/ar/Layout/Navbar';
+import ReferaPatient from '@/components/ar/Common/ReferaPatient';
+
+
+export const metadata: Metadata = {
+  title:  "طلب موعد مع طبيب – بنون في الرياض وجدة ",
+  description:
+    'اكتشفوا نسب نجاح عمليات أطفال الأنابيب والحقن المجهري في بنون. نسب حمل رائدة بفضل أحدث التقنيات وفريق طبي متميز.',
+};
+
+export default function ReferaPatientPage() {
+  return (
+    <>
+      <Navbar />
+
+      <OptimizedPageBanner imageName="referpaitent-banner-ar" />
+
+      <ReferaPatient />
+    </>
+  );
+}
