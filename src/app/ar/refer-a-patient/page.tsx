37e2c0d3d721cd5aa39e8e593b9fbcf5f4c0e0cc
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-22 05:42:10 +0500

    fix: change title

--- src/app/ar/refer-a-patient/page.tsx
+++ src/app/ar/refer-a-patient/page.tsx
@@ -5,7 +5,7 @@ import ReferaPatient from '@/components/ar/Common/ReferaPatient';
 
 
 export const metadata: Metadata = {
-  title:  "طلب موعد مع طبيب – بنون في الرياض وجدة ",
+  title:  "طلب تحويل مريض لرعاية متقدمة للخصوبة | بنــون ",
   description:
     'اكتشفوا نسب نجاح عمليات أطفال الأنابيب والحقن المجهري في بنون. نسب حمل رائدة بفضل أحدث التقنيات وفريق طبي متميز.',
 };
