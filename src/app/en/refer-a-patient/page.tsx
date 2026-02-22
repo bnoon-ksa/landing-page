2390bb9448ae814b458af6e891e5324a16b556e2
dubaiwebcity <dubaiwebcity@gmail.com>
2026-02-22 05:39:46 +0500

    fix: change title

--- src/app/en/refer-a-patient/page.tsx
+++ src/app/en/refer-a-patient/page.tsx
@@ -4,7 +4,7 @@ import Navbar from '@/components/Layout/Navbar';
 import ReferaPatient from '@/components/Common/ReferaPatient';
 import { CDN_URL } from '../../../utils/cdn';
 export const metadata: Metadata = {
-  title: 'Refer a Patient – Bnoon | Riyadh & Jeddah',
+  title: 'Refer a Patient for Expert Fertility Care | Bnoon ',
   description:
     "Learn about Bnoon, Saudi Arabia's leading fertility and women's health network. Advanced IVF and reproductive medicine across Riyadh, Jeddah, and Al Ahsa.",
 };
