8bd5ff9e7cf07fb47cee2454fd64b23a98558814
dubaiwebcity <dubaiwebcity@gmail.com>
2026-03-12 15:52:42 +0500

    fix: change text

--- src/components/ar/Common/Brochures.tsx
+++ src/components/ar/Common/Brochures.tsx
@@ -9,46 +9,51 @@ const brochures = [
     section_ar: "سحب البويضات",
     items: [
       {
-        title_en: "Egg Retrieval – Pre Operative",
-        title_ar: "سحب البويضات - قبل العملية",
+        title_en: "PRE-OPERATIVE INSTRUCTIONS: EGG RETRIEVAL",
+        title_ar: "تعـليـمـات مـا قبل سحـب البـويـضـات ",
         image: "https://bnoon-website.b-cdn.net/images/brochures/pre-egg-retrieval.jpg",
         pdf: "/ar/egg-retrieval-pre-operative",
+        downloadpdf: "/pdf/pre-operative-instructions-egg retrieval.pdf",
       },
       {
-        title_en: "Egg Retrieval – Post Operative",
-        title_ar: "سحب البويضات - بعد العملية",
+        title_en: "POST-OPERATIVE INSTRUCTIONS: EGG RETRIEVAL",
+        title_ar: "تـعـلـيـمات ما بعد سحـب البـويـضـات",
         image: "https://bnoon-website.b-cdn.net/images/brochures/post-egg-retrieval.jpg",
         pdf: "/ar/egg-retrieval-post-operative",
+        downloadpdf: "/pdf/post-operative-instructions-egg retrieval.pdf",
       },
     ],
   },
-  {
+  { 
     section_en: "Embryo Transfer",
     section_ar: "نقل الأجنة",
     items: [
       {
-        title_en: "Embryo Transfer – Pre Operative ",
-        title_ar: "نقل الأجنة - قبل العملية",
+        title_en: "PRE-OPERATIVE INSTRUCTIONS: EMBRYO TRANSFER ",
+        title_ar: "تعـليـمـات مـا قبل إرجـــاع األجـــنـــة",
         image: "https://bnoon-website.b-cdn.net/images/brochures/pre-embryo-transfer.jpg",
         pdf: "/ar/embryo-transfer-pre-operative",
+        downloadpdf: "/pdf/pre-operative- instructions- enbryo-transfer.pdf",
       },
       {
-        title_en: "Embryo Transfer – Post Operative",
-        title_ar: "نقل الأجنة - بعد العملية",
+        title_en: "POST-OPERATIVE INSTRUCTIONS: EMBRYO TRANSFER",
+        title_ar: "تـعـلـيـمات ما بعد إرجـــاع األجـــنـــة",
         image: "https://bnoon-website.b-cdn.net/images/brochures/post-embryo-transfer.jpg",
         pdf: "/ar/embryo-transfer-post-operative",
+        downloadpdf: "/pdf/post-operative- instructions- enbryo-transfer.pdf",
       },
     ],
   },
   {
-    section_en: "Semen Collection",
+    section_en: "Semen Collection", 
     section_ar: "جمع السائل المنوي",
     items: [
       {
-        title_en: "Semen Collection Instructions",
-        title_ar: "تعليمات جمع السائل المنوي",
+        title_en: "SEMEN COLLECTION INSTRUCTIONS",
+        title_ar: "تعليمات جمع عينة السائل المنوي",
         image: "https://bnoon-website.b-cdn.net/images/brochures/semen.jpg",
         pdf: "/ar/semen-collection-instructions",
+        downloadpdf: "/pdf/semen-collection-instructions.pdf",
       },
     ],
   },
@@ -61,18 +66,18 @@ const Brochures = () => {
   };
 
   return (
-    <div className="fertility-area mt-3 mt-lg-5 mb-3" dir="rtl">
-      <div className="container">
+    <div className="fertility-area bg-color" dir="rtl">
+      <div className="container mt-5 mb-5 ">
         {brochures.map((section, i) => (
           <div key={i} className="section-title mb-5">
- <div key={i} className="left">
-            {/* Arabic Heading */}
-            <h2 className="mb-1 fw-bold text-center">{section.section_ar}</h2>
+            <div key={i} className="left">
+              {/* Arabic Heading */}
+              <h2 className="mb-1 fw-bold text-center">{section.section_ar}</h2>
 
-            {/* English Heading */}
-            <h2 className="mb-4 fw-bold text-center">{section.section_en}</h2>
-</div>
-            <div className="row g-4 justify-content-center">
+              {/* English Heading */}
+              <h2 className="mb-4 fw-bold text-center">{section.section_en}</h2>
+            </div>
+            <div className="row g-5 justify-content-center">
               {section.items.map((item, index) => (
                 <div className="col-lg-5 col-md-6" key={index}>
                   <motion.div
@@ -81,12 +86,7 @@ const Brochures = () => {
                     whileInView="visible"
                     transition={{ duration: 0.5 }}
                   >
-                    <a
-                      href={item.pdf}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="brochure-card d-block text-center p-3"
-                    >
+                 <div className="brochure-card p-3">
                       <div className="brochure-image mb-3">
                         <img
                           src={item.image}
@@ -94,11 +94,29 @@ const Brochures = () => {
                           className="img-fluid rounded"
                         />
                       </div>
+<div className="d-flex justify-content-between align-items-center" dir="ltr">
 
-                      
-                        <h6 className="fw-semibold mb-1">{item.title_ar}</h6>
-                        <h6 className="fw-semibold mb-1">{item.title_en}</h6>
-                    </a>
+  <div className="text-start">
+    <a
+      href={item.pdf}
+      className="text-link"
+    >
+      <h6 className="fw-semibold mb-1 brochures">{item.title_ar}</h6>
+      <h6 className="fw-semibold mb-1 brochures">{item.title_en}</h6>
+    </a>
+  </div>
+
+  <a
+    href={item.downloadpdf}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="download-icon"
+  >
+   <img src="https://bnoon-website.b-cdn.net/images/icons/downloading.png" alt="download" />
+  </a>
+
+</div>
+                    </div>
                   </motion.div>
                 </div>
               ))}
@@ -109,6 +127,12 @@ const Brochures = () => {
 
       {/* CSS inside same file */}
       <style jsx>{`
+      .bg-color{
+          background-color: #d7f2fb;
+      }
+      .brochures{
+      color: #004E78;
+      }
         .brochure-card {
           background: #ffffff;
           border-radius: 14px;
@@ -143,6 +167,15 @@ const Brochures = () => {
           font-size: 16px;
           color: #111;
         }
+          .download-icon {
+  color: #004E78;
+  width: 35px;
+  margin-left: 10px;
+}
+
+.download-icon:hover {
+  color: #0a6ea8;
+}
       `}</style>
     </div>
   );
