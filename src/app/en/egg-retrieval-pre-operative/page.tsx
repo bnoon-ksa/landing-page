bc29f87bf6fd5d19675c945779398acec0434232
dubaiwebcity <dubaiwebcity@gmail.com>
2026-03-12 18:40:45 +0500

    fix: add pre page

---
+++ src/app/en/egg-retrieval-pre-operative/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import Navbar from '@/components/Layout/Navbar';
+import PreEggRetrieval from '@/components/Common/PreEggRetrieval';
+
+export const metadata: Metadata = {
+  title: 'Patients Rights – Bnoon',
+  description:
+    'Learn about your rights as a patient at Bnoon. We are committed to transparent, fair medical care that respects your privacy and dignity.',
+};
+
+export default function ServerPatientsRightsPage() {
+  return (
+    <div className="">
+      <Navbar />
+      <PreEggRetrieval />
+    </div>
+  );
+}
