87f8006677cee71ae6fff1fb4f83cc9ec84f1856
dubaiwebcity <dubaiwebcity@gmail.com>
2026-03-12 18:58:59 +0500

    fix: add pre page

---
+++ src/app/en/embryo-transfer-pre-operative/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import Navbar from '@/components/Layout/Navbar';
+import PreEmbryoTransfer from '@/components/Common/PreEmbryoTransfer';
+
+export const metadata: Metadata = {
+  title: 'Patients Rights – Bnoon',
+  description:
+    'Learn about your rights as a patient at Bnoon. We are committed to transparent, fair medical care that respects your privacy and dignity.',
+};
+
+export default function ServerPatientsRightsPage() {
+  return (
+    <div className="">
+      <Navbar />
+      <PreEmbryoTransfer />
+    </div>
+  );
+}
