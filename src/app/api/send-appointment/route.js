339fa8460b3ad80eeeb5b8001eabeaaeb4ba8525
dubaiwebcity <dubaiwebcity@gmail.com>
2026-03-18 19:47:02 +0500

    fix: change text

--- src/app/api/send-appointment/route.js
+++ src/app/api/send-appointment/route.js
@@ -25,6 +25,13 @@ export async function POST(req) {
         pass: "vpupjvfrntavidhw",
       },
     });
+  // ✅ TEST EMAIL (optional)
+    await transporter.sendMail({
+      from: "bnooninfo@gmail.com",
+      to: "appointments.jeddah@bnoon.sa",
+      subject: "Test",
+      text: "Test email",
+    });
 
     await transporter.sendMail({
       from: `"Appointment Request" <${process.env.SMTP_USER}>`,
