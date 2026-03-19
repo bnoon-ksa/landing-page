57a18fd3c7723b76b24cf51f7f557acb98230948
dubaiwebcity <dubaiwebcity@gmail.com>
2026-03-19 17:10:37 +0500

    fix: change text (#354)

--- src/app/api/referrals/route.js
+++ src/app/api/referrals/route.js
@@ -3,17 +3,13 @@ import ReferralEN from "../../../models/ReferralEN";
 import nodemailer from "nodemailer";
 
 export async function POST(req) {
- 
   try {
     const data = await req.json();
-        // ✅ DB SAFE
-    try {
-      await connectDB();
-      await ReferralEN.create(data);
-      console.log("✅ Data saved to MongoDB");
-    } catch (dbError) {
-      console.error("❌ DB ERROR:", dbError);
-    }
+
+    // ✅ DB connect + save
+    await connectDB();
+    const saved = await ReferralEN.create(data);
+
     // ✅ Recipient mapping based on selected branch
     const RECIPIENTS = {
       "Bnoon – Jeddah": "referral.jeddah@bnoon.sa",
